b8239e8dbc2978823e1a973eb583299133e76f91
Amanda Aragão <amandaaragao72@gmail.com>
2023-03-17 16:22:14 -0300

    Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
 import './App.css';
 // import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
+import Home from './pages/Home';
 
 class App extends React.Component {
   // componentDidMount() {
@@ -11,20 +12,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>Edit src/App.js and save to reload.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Route exact path="/" component={ Home } />
     );
   }
 }
