be5a30f4471f8b1290d5460e712c4d85e98b153f
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-18 10:22:24 -0300

    Requisito 06 em construção

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryId } from '../services/api';
 
 class Menu extends React.Component {
   state = {
     categories: [],
+    searchResults: [],
+    searchClicked: false,
   };
 
   componentDidMount() {
@@ -18,18 +20,35 @@ class Menu extends React.Component {
   };
 
   render() {
-    const { categories } = this.state;
+    const { categories, searchClicked, searchResults } = this.state;
+    // console.log(searchResults);
     return (
       <section>
         {categories.map((categorie) => (
           <label data-testid="category" key={ categorie.id } htmlFor="input-radio">
-            <input
-              type="radio"
-              id="input-radio"
-            />
-            { categorie.name }
+            <button
+              type="button"
+              onClick={ async () => {
+                const data = await getProductsFromCategoryId(categorie.id);
+                this.setState({
+                  searchResults: data.results,
+                  searchClicked: true,
+                });
+              } }
+            >
+              { categorie.name }
+            </button>
           </label>
         ))}
+        {
+          searchClicked && searchResults.map((product) => (
+            <div data-testid="product" key={ product.id }>
+              <h3>{ product.title }</h3>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <h3>{ product.price }</h3>
+            </div>
+          ))
+        }
       </section>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,20 @@ export async function getCategories() {
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery(query) {
+export async function getProductsFromQuery(query) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getProductsFromCategoryId(categoryId) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  const data = await response.json();
+  return data;
+}
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`);
   const data = await response.json();
   return data;
 }
