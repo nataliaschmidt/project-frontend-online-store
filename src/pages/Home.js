01dc32c6e79ca1371c670a682279a8a9eb68c438
nataliaschmidt <nataliak.schmidt@gmail.com>
2023-03-18 11:50:25 -0300

    Requisito 06

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Menu from '../components/Menu';
-import ProductsList from '../components/ProductsList';
-import { getProductsFromQuery } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends React.Component {
   state = {
-    search: '',
-    productList: {},
-    button: false,
+    categories: [],
+    searchInput: '',
+    searchResult: [],
+    searchClicked: false,
   };
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({
+      categories,
+    });
+  }
 
   handleChange = ({ target }) => {
     const { name, value } = target;
@@ -18,57 +24,138 @@ class Home extends React.Component {
     });
   };
 
-  fetchGetProductsFromQuery = async (search) => {
-    // const { search } = this.state;
-    const productsList = await getProductsFromQuery(search);
-    // console.log(productsList);
+  handleClick = async () => {
+    const { searchInput } = this.state;
+    const response = await getProductsFromCategoryAndQuery('', searchInput);
     this.setState({
-      productList: productsList,
-      button: true,
+      searchResult: response.results,
+      searchClicked: true,
     });
   };
 
   render() {
-    const { search, productList, button } = this.state;
+    const { categories, searchResult, searchClicked } = this.state;
     return (
-      <section>
-        <form>
-          <label>
-            <input
-              data-testid="query-input"
-              type="search"
-              name="search"
-              value={ search }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <button
-            data-testid="query-button"
-            type="button"
-            onClick={ () => this.fetchGetProductsFromQuery(search) }
-          >
-            Pesquisar
-          </button>
-        </form>
-        <p>
-          <Link
-            data-testid="shopping-cart-button"
-            to="/carrinho-de-compras"
-          >
-            Carrinho de compras.
-          </Link>
-        </p>
-        {
-          !search && (
-            <p data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </p>
+      <div>
+        {searchClicked === false
+          ? (
+            <>
+              {
+                categories.map((categorie) => (
+                  <button
+                    data-testid="category"
+                    key={ categorie.name }
+                    onClick={ async () => {
+                      const response = await
+                      getProductsFromCategoryAndQuery(categorie.id, '');
+                      this.setState({
+                        searchResult: response.results,
+                        searchClicked: true,
+                      });
+                    } }
+                  >
+                    {categorie.name}
+                  </button>
+                ))
+              }
+              <label>
+                <input
+                  type="text"
+                  data-testid="query-input"
+                  name="searchInput"
+                  onChange={ this.handleChange }
+                />
+                <button
+                  data-testid="query-button"
+                  onClick={ this.handleClick }
+                >
+                  Pesquisar
+                </button>
+              </label>
+              <Link
+                to="/carrinho-de-compras"
+                data-testid="shopping-cart-button"
+              >
+                Carrinho de Compras
+              </Link>
+              <p
+                data-testid="home-initial-message "
+              >
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </p>
+            </>
           )
-        }
-        {button && <ProductsList products={ productList } />}
-        <Menu />
-      </section>
+          : (
+            <>
+              {
+                categories.map((categorie) => (
+                  <button
+                    data-testid="category"
+                    key={ categorie.name }
+                    onClick={ async () => {
+                      const response = await
+                      getProductsFromCategoryAndQuery(categorie.id, '');
+                      this.setState({
+                        searchResult: response.results,
+                        searchClicked: true,
+                      });
+                    } }
+                  >
+                    {categorie.name}
+                  </button>
+                ))
+              }
+              <label>
+                <input
+                  type="text"
+                  data-testid="query-input"
+                  name="searchInput"
+                  onChange={ this.handleChange }
+                />
+                <button
+                  data-testid="query-button"
+                  onClick={ this.handleClick }
+                >
+                  Pesquisar
+                </button>
+              </label>
+              <Link
+                to="/cart"
+                data-testid="shopping-cart-button"
+              >
+                Carrinho de Compras
+              </Link>
+              {
+                searchResult.length > 0 ? (
+                  <ul>
+                    {
+                      searchResult.map((result) => (
+                        <li
+                          key={ result.id }
+                          data-testid="product"
+                        >
+                          <Link
+                            data-testid="product-detail-link"
+                            to={ `/product/${result.id}` }
+                          >
+                            <h2>{result.title}</h2>
+                            <img src={ `${result.thumbnail}` } alt="product-img" />
+                            <p>
+                              R$:
+                              {` ${result.price}`}
+                            </p>
+                          </Link>
+                        </li>
+                      ))
+                    }
+                  </ul>
+                ) : <p>Nenhum produto foi encontrado</p>
+              }
+            </>
+          )}
+      </div>
     );
   }
 }
+
 export default Home;
--- src/services/api.js
+++ src/services/api.js
@@ -4,20 +4,18 @@ export async function getCategories() {
   return data;
 }
 
-export async function getProductsFromQuery(query) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
-  const data = await response.json();
-  return data;
-}
-
-export async function getProductsFromCategoryId(categoryId) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
-  const data = await response.json();
-  return data;
-}
-
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`);
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if (categoryId.length > 0 && query.length > 0) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const data = await response.json();
+    return data;
+  } if (categoryId.length > 0 && query.length === 0) {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const data = await response.json();
+    return data;
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=${query}`);
   const data = await response.json();
   return data;
 }
